7e78dbeff595596ca5721fa77f1c85790bf2a6f2
March012847 <tur830106@tkt.ac.th>
2025-07-18 11:13:00 +0700

    Update eat.lua

--- eat.lua
+++ eat.lua
@@ -6,6 +6,10 @@ local ltn12 = require("ltn12")
 local http = require("socket.http")
 local dkjson = require("dkjson")
 
+-- varibles
+local webhooks = nil
+local reponse = {}
+
 -- main function
 function eat:StartEating(person, eater)
   print("Start eating " .. person .. " now")
@@ -32,5 +36,41 @@ Hello and welcome to the eat package (version 0.0.1-beta). This package is a jok
    - Simulates eating a person with progress output.
 ]])
 end
+
+
+
+function eat:registerurl(url)
+  print("registered webhook ".. url)
+  webhooks = url
+end
+
+function eat:PostEating(person, eater)
+  local not_encoded = {
+    embeds = {
+      {
+        title = "Person was eaten",
+        description = person .. " was eaten by " .. eater,
+        color = 16711680
+      }
+    }
+  }
+
+  local encoded = dkjson.encode(not_encoded)
+  local response = {}
+
+  http.request{
+    url = webhooks,
+    method = "POST",
+    headers = {
+      ["Content-Type"] = "application/json", -- lowercase "application"
+      ["Content-Length"] = tostring(#encoded), -- needs to be a string
+    },
+    source = ltn12.source.string(encoded),
+    sink = ltn12.sink.table(response)
+  }
+
+  print("Webhook sent! Response: ", table.concat(response))
+end
+
 
 return eat
